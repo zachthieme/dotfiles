c3f02b1f82298fdfd81b9b606182443af88b541a
zachthieme <zach@techsage.org>
2022-09-15 14:08:28 -0700

    fixed git path in aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -22,8 +22,8 @@ zoxide init fish | source
 alias dot='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 
 # find files changed in the .config directory in the last day 
-alias nf='fdfind . /home/zach/.config -H --changed-within 1d -E Code -E google-chrome -x /home/zach/.nix-profile/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME ls-files --others {} | sort | uniq'
-alias nfa='fdfind . /home/zach/.config -H --changed-within 1d -E chromium -x /home/zach/.nix-profile/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME ls-files --others {} | uniq | rargs /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME add {0}'
+alias nf='fdfind . /home/zach/.config -H --changed-within 1d -E Code -E google-chrome -x /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME ls-files --others {} | sort | uniq'
+alias nfa='fdfind . /home/zach/.config -H --changed-within 1d -E chromium -x /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME ls-files --others {} | uniq | rargs /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME add {0}'
 
 # make exa less typing
 alias exa='exa --group-directories-first'
