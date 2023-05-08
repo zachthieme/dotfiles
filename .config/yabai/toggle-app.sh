e21b54df0ed510a91f074a1992fbf9214b822afd
zachthieme <zach@techsage.org>
2023-05-08 15:16:48 -0700

    started apps if they weren't running

--- .config/yabai/toggle-app.sh
+++ .config/yabai/toggle-app.sh
@@ -4,6 +4,13 @@ APP=$1
 ID=$(yabai -m query --windows --space | jq -r  --arg APP "$APP" '.[] | select(.app ==$APP) | .id ') 
 MINIMIZED=$(yabai -m query --windows --space | jq -r  --arg APP "$APP" '.[] | select(.app ==$APP) | ."is-minimized"') 
 
+if [ -n "$ID"]
+then
+  open -a $APP
+  sleep 1
+fi
+
+
 if [ "true" == "$MINIMIZED" ]
 then
   yabai -m window --deminimize  $ID
