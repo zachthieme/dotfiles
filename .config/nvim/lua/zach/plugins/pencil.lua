7b5f63f1af989b9a5985eb295b2a4b054ed06e08
zachthieme <zach@techsage.org>
2024-06-04 22:32:32 -0700

    added new leader-vs command to create writing split

--- .config/nvim/lua/zach/plugins/pencil.lua
+++ .config/nvim/lua/zach/plugins/pencil.lua
@@ -3,4 +3,13 @@ return {
   init = function()
     vim.g["pencil#wrapModeDefault"] = "soft"
   end,
+  config = function()
+    local keymap = vim.keymap
+    keymap.set(
+      "n",
+      "<leader>vs",
+      "<cmd>:topleft vs <BAR> vertical resize 50<cr>",
+      { desc = "create split to the left" }
+    )
+  end,
 }
