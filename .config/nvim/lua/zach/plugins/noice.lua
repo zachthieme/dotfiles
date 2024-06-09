9f745d92dd97862e64b357e0f55efe24cdc26ec8
Zach Thieme <zach@techsage.org>
2024-06-08 19:01:50 -0700

    added buffers to telescope, added new element to lua line to show when macro's are being recorded

--- .config/nvim/lua/zach/plugins/noice.lua
+++ .config/nvim/lua/zach/plugins/noice.lua
@@ -6,15 +6,4 @@ return {
     "MunifTanjim/nui.nvim",
     "rcarriga/nvim-notify",
   },
-  require("lualine").setup({
-  sections = {
-    lualine_x = {
-      {
-        require("noice").api.statusline.mode.get,
-        cond = require("noice").api.statusline.mode.has,
-        color = { fg = "#ff9e64" },
-      }
-    },
-  },
-})
 }
