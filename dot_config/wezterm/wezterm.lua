149fc0326fecad0fe83454f5ba9532572b39b0c0
Zach Thieme <zach@techsage.org>
2024-06-17 10:02:35 -0700

    Add .config/wezterm/wezterm.lua

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require("wezterm")
+
+return {
+  -- ...your existing config
+  color_scheme = "Catppuccin Mocha", -- or Macchiato, Frappe, Latte
+  hide_tab_bar_if_only_one_tab = true,
+  -- font = wezterm.font("JetBrains Mono"),
+  font = wezterm.font("Hack Nerd Font Mono"),
+}
