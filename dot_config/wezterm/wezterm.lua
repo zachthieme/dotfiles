463d45830a8183df03d8fd44b7fbc8f8b61da3ce
Zach Thieme <zach@techsage.org>
2024-06-17 17:14:11 -0700

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -5,5 +5,7 @@ return {
   color_scheme = "Catppuccin Mocha", -- or Macchiato, Frappe, Latte
   hide_tab_bar_if_only_one_tab = true,
   -- font = wezterm.font("JetBrains Mono"),
-  font = wezterm.font("JetBrains Mono", { weight = "Bold" }),
+  -- font = wezterm.font_with_fallback({ "Hack Nerd Font Mono", "JetBrains Mono" }),
+  wezterm.font("Roboto", { weight = 250, stretch = "Normal", style = "Normal" }),
+  -- font = wezterm.font_with_fallback({ "JetBrains Mono", "Hack Nerd Font Mono" }),
 }
