73c212a90458ec048872c316d3d63ec7e9ea550e
Zach Thieme <zach@techsage.org>
2024-06-18 16:32:36 -0700

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,11 +1,8 @@
 local wezterm = require("wezterm")
 
 return {
-  -- ...your existing config
-  color_scheme = "Catppuccin Mocha", -- or Macchiato, Frappe, Latte
-  hide_tab_bar_if_only_one_tab = true,
-  -- font = wezterm.font("JetBrains Mono"),
-  -- font = wezterm.font_with_fallback({ "Hack Nerd Font Mono", "JetBrains Mono" }),
-  wezterm.font("Roboto", { weight = 250, stretch = "Normal", style = "Normal" }),
-  -- font = wezterm.font_with_fallback({ "JetBrains Mono", "Hack Nerd Font Mono" }),
+  font = wezterm.font_with_fallback({
+    { family = "Hack Nerd Font Mono", weight = "Bold" },
+    { family = "JetBrains Mono", weight = "Bold" },
+  }),
 }
