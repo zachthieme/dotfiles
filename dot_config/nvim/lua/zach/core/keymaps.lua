a5aefc9cc984fe7ec78aaf0a074be07f6d579677
Zach Thieme <zach@techsage.org>
2024-06-18 21:57:26 -0700

    Update .config/nvim/lua/zach/core/keymaps.lua

--- dot_config/nvim/lua/zach/core/keymaps.lua
+++ dot_config/nvim/lua/zach/core/keymaps.lua
@@ -34,10 +34,9 @@ keymap.set("n", "<leader>a", "ggVG<CR>", { desc = "Select all" }) -- select all 
 keymap.set("v", "<leader>c", '"*y<CR>', { desc = "Copy to system clipboard" }) -- copy to system clipboard
 keymap.set("n", "<C-d>", "<C-d>zz", { desc = "move down and center" })
 keymap.set("n", "<C-u>", "<C-u>zz", { desc = "move up and center" })
-keymap.set("n", "<leader>zz", "<cmd> Pencil | TZMinimalist<CR>", { desc = "testing stuff" })
 keymap.set(
   "n",
   "<leader>wn",
-  "<cmd>:topleft vs <BAR>vertical resize 50 <BAR>norm Gzt<cr> <BAR><cmd>Pencil <BAR>TZMinimalist<cr>",
+  "<cmd>:Pencil <BAR>TZMinimalist <BAR>topleft vs <BAR>vertical resize 50 <BAR>norm Gzt<cr> ",
   { desc = "Setup my writing environment." }
 )
