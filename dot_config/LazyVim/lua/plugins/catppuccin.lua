d944e9d6fc398a29cd84920008abc178c7370ad6
Zach Thieme <zach@techsage.org>
2024-06-25 20:32:46 -0700

    Update .config/LazyVim/lua/plugins/catppuccin.lua

--- dot_config/LazyVim/lua/plugins/catppuccin.lua
+++ dot_config/LazyVim/lua/plugins/catppuccin.lua
@@ -1,1 +1,10 @@
 return { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+
+require("catppuccin").setup({
+    flavour = "auto", -- latte, frappe, macchiato, mocha
+})
+
+-- setup must be called before loading
+vim.cmd.colorscheme "catppuccin"
+
+
