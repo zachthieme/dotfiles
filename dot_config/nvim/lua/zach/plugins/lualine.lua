05cf652a4858159bca7121e5db53f7cd64c68954
Zach Thieme <zach@techsage.org>
2024-06-27 22:25:32 -0700

    Update .config/nvim/lua/zach/plugins/lualine.lua

--- dot_config/nvim/lua/zach/plugins/lualine.lua
+++ dot_config/nvim/lua/zach/plugins/lualine.lua
@@ -1,6 +1,7 @@
 return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
+
   config = function()
     local lualine = require("lualine")
     local lazy_status = require("lazy.status") -- to configure lazy pending updates count
@@ -52,7 +53,7 @@ return {
     -- configure lualine with modified theme
     lualine.setup({
       options = {
-        theme = my_lualine_theme,
+        theme = "catppuccin",
       },
       sections = {
         lualine_b = {
