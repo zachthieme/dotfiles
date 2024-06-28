7e808048b8452c3ea52349153a3819c28e6f578b
Zach Thieme <zach@techsage.org>
2024-06-27 22:18:16 -0700

    Update .config/nvim/lua/zach/plugins/catppuccin.lua

--- dot_config/nvim/lua/zach/plugins/catppuccin.lua
+++ dot_config/nvim/lua/zach/plugins/catppuccin.lua
@@ -5,20 +5,19 @@ return {
     priority = 1000,
     config = function()
       vim.cmd([[colorscheme catppuccin]])
+      require("catppuccin").setup({
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          nvimtree = true,
+          treesitter = true,
+          notify = false,
+          -- mini = {
+          --     enabled = true,
+          --     indentscope_color = "",
+          -- },
+        },
+      })
     end,
-
-    -- require("catppuccin").setup({
-    --   integrations = {
-    --     cmp = true,
-    --     gitsigns = true,
-    --     nvimtree = true,
-    --     treesitter = true,
-    --     notify = false,
-    --     -- mini = {
-    --     --     enabled = true,
-    --     --     indentscope_color = "",
-    --     -- },
-    --   },
-    -- }),
   },
 }
