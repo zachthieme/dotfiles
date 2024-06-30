2de00f69d293d0f6c96a77097d7a4d1917ac4a12
Zach Thieme <zach@techsage.org>
2024-06-30 15:21:32 -0700

    Update .config/nvim/lua/zach/core/options.lua

--- dot_config/nvim/lua/zach/core/options.lua
+++ dot_config/nvim/lua/zach/core/options.lua
@@ -20,7 +20,6 @@ opt.smartcase = true -- if you include mixed case in your search, assumes you wa
 -- cursor line
 opt.cursorline = true -- highlight the current cursor line
 -- opt.guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20"
-opt.guicursor = "n-v:ver25,i-c:hor20,r-cr-o:hor20"
 -- appearance
 
 -- turn on termguicolors for nightfly colorscheme to work
@@ -29,6 +28,7 @@ opt.termguicolors = true
 opt.background = "dark" -- colorschemes that can be light or dark will be made dark
 opt.signcolumn = "yes" -- show sign column so that text doesn't shift
 
+opt.guicursor = "n-v:ver25,i-c:hor20,r-cr-o:hor20"
 -- backspace
 opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
 
