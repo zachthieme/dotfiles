8f2cd5116eb00532cb837248533ee9d43c3e3955
Zach Thieme <zach@techsage.org>
2024-06-30 17:50:08 -0700

    Update .config/nvim/lua/zach/core/options.lua

--- dot_config/nvim/lua/zach/core/options.lua
+++ dot_config/nvim/lua/zach/core/options.lua
@@ -19,7 +19,7 @@ opt.smartcase = true -- if you include mixed case in your search, assumes you wa
 
 -- cursor line
 opt.cursorline = true -- highlight the current cursor line
--- opt.guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20"
+opt.guicursor = { "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20" }
 -- appearance
 
 -- turn on termguicolors for nightfly colorscheme to work
