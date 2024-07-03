8ae36ca29f97688ea1f2098803751c76de5c642f
Zach Thieme <zach@techsage.org>
2024-07-02 22:59:59 -0700

    Update .config/nvim/lua/zach/plugins/formatting.lua

--- dot_config/nvim/lua/zach/plugins/formatting.lua
+++ dot_config/nvim/lua/zach/plugins/formatting.lua
@@ -7,20 +7,13 @@ return {
 
     conform.setup({
       formatters_by_ft = {
-        javascript = { "prettier" },
-        typescript = { "prettier" },
-        javascriptreact = { "prettier" },
-        typescriptreact = { "prettier" },
-        svelte = { "prettier" },
         css = { "prettier" },
         html = { "prettier" },
         json = { "prettier" },
         yaml = { "prettier" },
         markdown = { "prettier" },
-        graphql = { "prettier" },
         lua = { "stylua" },
-        --          python = { "isort", "black" },
-        go = { "gofmt", "govent" },
+        go = { "gofmt" },
       },
       format_on_save = {
         lsp_fallback = true,
