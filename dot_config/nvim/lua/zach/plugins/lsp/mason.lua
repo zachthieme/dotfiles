d6726134cfb793986e889b0cb1a8b9e644423c4c
Zach Thieme <zach@techsage.org>
2024-11-14 20:14:49 -0800

    Update .config/nvim/lua/zach/plugins/lsp/mason.lua

--- dot_config/nvim/lua/zach/plugins/lsp/mason.lua
+++ dot_config/nvim/lua/zach/plugins/lsp/mason.lua
@@ -38,6 +38,7 @@ return {
 				"prismals",
 				"pyright",
 				"gopls",
+				"proselint",
 			},
 			-- auto-install configured servers (with lspconfig)
 			automatic_installation = true, -- not the same as ensure_installed
