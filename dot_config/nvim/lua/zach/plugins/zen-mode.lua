c57eccd24ef9a80c3196192e493529cff01543f6
Zach Thieme <zach@techsage.org>
2024-11-14 20:40:33 -0800

    Update .config/nvim/lua/zach/plugins/zen-mode.lua

--- dot_config/nvim/lua/zach/plugins/zen-mode.lua
+++ dot_config/nvim/lua/zach/plugins/zen-mode.lua
@@ -12,7 +12,7 @@ return {
 				height = 1, -- height of the Zen window
 				options = {
 					signcolumn = "no", -- disable signcolumn
-					number = false, -- disable number column
+					number = true, -- disable number column
 					relativenumber = false, -- disable relative numbers
 					-- cursorline = false, -- disable cursorline
 					-- cursorcolumn = false, -- disable cursor column
