cf1bb3b33597a6095bc37050ae68764213f0875c
Zach Thieme <zach@techsage.org>
2024-11-14 21:10:37 -0800

    Update .config/nvim/lua/zach/plugins/zen-mode.lua

--- dot_config/nvim/lua/zach/plugins/zen-mode.lua
+++ dot_config/nvim/lua/zach/plugins/zen-mode.lua
@@ -1,6 +1,8 @@
 return {
 	"folke/zen-mode.nvim",
 	opts = {
+		number = false, -- disable number column
+		relativenumber = false, -- disable relative numbers
 		-- your configuration comes here
 		-- or leave it empty to use the default settings
 		-- refer to the configuration section below
