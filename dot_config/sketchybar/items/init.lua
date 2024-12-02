e945d2e939018476e25b6f7fa7784f49c32ad5a4
Zach Thieme <zach@techsage.org>
2024-12-02 12:59:27 -0800

    Update .config/sketchybar/items/init.lua

--- dot_config/sketchybar/items/init.lua
+++ dot_config/sketchybar/items/init.lua
@@ -1,6 +1,7 @@
-require("items.apple")
+-- require("items.apple")
 require("items.spaces")
 require("items.front_app")
 require("items.calendar")
 require("items.widgets")
 -- require("items.media")
+
