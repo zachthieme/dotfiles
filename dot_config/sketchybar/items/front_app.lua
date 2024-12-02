8dc2590de65ba81292657bd39eb9b99260308a7b
Zach Thieme <zach@techsage.org>
2024-12-02 14:02:03 -0800

    Update .config/sketchybar/items/front_app.lua

--- dot_config/sketchybar/items/front_app.lua
+++ dot_config/sketchybar/items/front_app.lua
@@ -2,7 +2,7 @@ local colors = require("colors")
 local settings = require("settings")
 local icons = require("icons")
 
-local icon_color = colors.grey
+local icon_color = colors.green
 local icon_string = "?"
 
 local front_app = sbar.add("item", "front_app", {
