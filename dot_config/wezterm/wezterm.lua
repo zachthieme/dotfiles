0eb8b026bc51ab0cb9df5ecf68c93887ab14dd34
Zach Thieme <zach@techsage.org>
2024-12-02 14:45:49 -0800

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,11 +1,13 @@
 local wezterm = require("wezterm")
 
-return {
-  color_scheme = "Catppuccin Mocha",
-  font = wezterm.font_with_fallback({
-    { family = "Hack Nerd Font Mono", weight = "Bold" },
-    { family = "JetBrains Mono", weight = "Bold" },
-    enable_tab_bar = false,
-    window_decorations = "RESIZE",
-  }),
-}
+local config = wezterm.config_builder()
+
+config.color_scheme = "Catppuccin Mocha"
+config.font = wezterm.font_with_fallback({
+  { family = "Hack Nerd Font Mono", weight = "Bold" },
+  { family = "JetBrains Mono", weight = "Bold" },
+})
+config.enable_tab_bar = false
+config.window_decorations = "RESIZE"
+
+return config
