cd237eea6970c7ce7f74aaeb722f3972d78df4c2
Zach Thieme <zach@techsage.org>
2024-12-02 13:47:26 -0800

    Update .config/sketchybar/items/widgets/battery.lua

--- dot_config/sketchybar/items/widgets/battery.lua
+++ dot_config/sketchybar/items/widgets/battery.lua
@@ -3,16 +3,16 @@ local colors = require("colors")
 local settings = require("settings")
 
 local battery = sbar.add("item", "widgets.battery", {
-  position = "right",
+  position = "q",
   icon = {
     font = {
       style = settings.font.style_map["Regular"],
       size = 19.0,
-    }
+    },
   },
   label = { font = { family = settings.font.numbers } },
   update_freq = 180,
-  popup = { align = "center" }
+  popup = { align = "center" },
 })
 
 local remaining_time = sbar.add("item", {
@@ -20,17 +20,16 @@ local remaining_time = sbar.add("item", {
   icon = {
     string = "Time remaining:",
     width = 100,
-    align = "left"
+    align = "left",
   },
   label = {
     string = "??:??h",
     width = 100,
-    align = "right"
+    align = "right",
   },
 })
 
-
-battery:subscribe({"routine", "power_source_change", "system_woke"}, function()
+battery:subscribe({ "routine", "power_source_change", "system_woke" }, function()
   sbar.exec("pmset -g batt", function(batt_info)
     local icon = "!"
     local label = "?"
@@ -70,7 +69,7 @@ battery:subscribe({"routine", "power_source_change", "system_woke"}, function()
     battery:set({
       icon = {
         string = icon,
-        color = color
+        color = color,
       },
       label = { string = lead .. label },
     })
@@ -79,22 +78,22 @@ end)
 
 battery:subscribe("mouse.clicked", function(env)
   local drawing = battery:query().popup.drawing
-  battery:set( { popup = { drawing = "toggle" } })
+  battery:set({ popup = { drawing = "toggle" } })
 
   if drawing == "off" then
     sbar.exec("pmset -g batt", function(batt_info)
       local found, _, remaining = batt_info:find(" (%d+:%d+) remaining")
       local label = found and remaining .. "h" or "No estimate"
-      remaining_time:set( { label = label })
+      remaining_time:set({ label = label })
     end)
   end
 end)
 
 sbar.add("bracket", "widgets.battery.bracket", { battery.name }, {
-  background = { color = colors.bg1 }
+  background = { color = colors.bg1 },
 })
 
 sbar.add("item", "widgets.battery.padding", {
-  position = "right",
-  width = settings.group_paddings
+  position = "q",
+  width = settings.group_paddings,
 })
