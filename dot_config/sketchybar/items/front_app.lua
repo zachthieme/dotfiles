abc302c9b01eff482a3a1dd27bd93dbd66d04059
Zach Thieme <zach@techsage.org>
2024-12-11 15:20:20 -0800

    Update .config/sketchybar/items/front_app.lua

--- dot_config/sketchybar/items/front_app.lua
+++ dot_config/sketchybar/items/front_app.lua
@@ -10,7 +10,7 @@ local front_app = sbar.add("item", "front_app", {
   icon = { drawing = false },
   position = "q",
   label = {
-    color = colors.grey,
+    color = colors.darkgrey,
     font = {
       style = settings.font.style_map["Black"],
       size = 12.0,
