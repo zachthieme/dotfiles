d0e595852748ea962945f54926b8481d5dfa3348
Zach Thieme <zach@techsage.org>
2024-12-27 19:38:22 -0800

    making .fzf.zsh portable

--- dot_fzf.zsh
+++ dot_fzf.zsh
@@ -1,7 +1,11 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/test/.fzf/bin* ]]; then
-  PATH="${PATH:+${PATH}:}/home/test/.fzf/bin"
+if [[ ! "$PATH" == *"$HOME/.fzf/bin"* ]]; then
+  PATH="${PATH:+${PATH}:}$HOME/.fzf/bin"
 fi
+
+# if [[ ! "$PATH" == */home/test/.fzf/bin* ]]; then
+#   PATH="${PATH:+${PATH}:}/home/test/.fzf/bin"
+# fi
 
 source <(fzf --zsh)
