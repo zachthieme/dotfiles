c3ab5084e6f5c3259cd270142074fccdec7d256b
Zach Thieme <zach@techsage.org>
2024-12-28 15:07:31 -0800

    nightfly

--- dot_config/nvim/lua/zach/plugins/nightfly.lua
+++ dot_config/nvim/lua/zach/plugins/nightfly.lua
@@ -1,11 +1,12 @@
 return {
   {
     'bluz71/vim-nightfly-colors',
-    -- lazy = true, -- Optional: ensures the theme is loaded immediately
-    -- priority = 1000, -- Optional: ensures the theme is loaded first
-    config = function()
-      -- vim.cmd([[colorscheme nightfly]])
-      vim.cmd('colorscheme nightfly')
-    end,
+    lazy = false, -- Optional: ensures the theme is loaded immediately
+    priority = 1000, -- Optional: ensures the theme is loaded first
+    -- config = function()
+    --   -- vim.cmd([[colorscheme nightfly]])
+    --   vim.cmd('colorscheme nightfly')
+    -- end,
   },
 }
+
