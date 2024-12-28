e979f188daededbdbffa8e4e11dbbab4e6973552
Zach Thieme <zach@techsage.org>
2024-12-27 21:23:45 -0800

    updated lua line

--- dot_config/nvim/lua/zach/plugins/lualine.lua
+++ dot_config/nvim/lua/zach/plugins/lualine.lua
@@ -7,7 +7,7 @@ return {
     local lazy_status = require("lazy.status") -- to configure lazy pending updates count
     lualine.setup({
       options = {
-        theme = "nightfly",
+        theme = "catppuccin"
       },
       sections = {
         lualine_b = {
