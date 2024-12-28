bb0c7721cdd24aa16c0f8773a465713ea7b8b161
Zach Thieme <zach@techsage.org>
2024-12-28 14:08:26 -0800

    still tweaking terminal

---
+++ dot_config/zsh/themes/nightfly_zsh-history-autosuggest.zsh
@@ -0,0 +1,41 @@
+# Nightfly Theme Colors
+# Background: #011627 (dark navy)
+# Foreground: #acb4c2 (soft white)
+# Highlight colors:
+# - Light blue: #82aaff
+# - Purple: #c792ea
+# - Teal: #7fdbca
+# - Yellow: #ecc48d
+# - Red: #f07178
+# - Muted gray-blue: #7e8e9e
+
+### zsh-syntax-highlighting Nightfly Theme ###
+ZSH_HIGHLIGHT_STYLES[default]='fg=252'
+ZSH_HIGHLIGHT_STYLES[command]='fg=81'
+ZSH_HIGHLIGHT_STYLES[unknown-command]='fg=131'
+ZSH_HIGHLIGHT_STYLES[reserved-word]='fg=179'
+ZSH_HIGHLIGHT_STYLES[builtin]='fg=110'
+ZSH_HIGHLIGHT_STYLES[alias]='fg=140'
+ZSH_HIGHLIGHT_STYLES[function]='fg=81'
+ZSH_HIGHLIGHT_STYLES[path]='fg=110'
+ZSH_HIGHLIGHT_STYLES[path_prefix]='fg=67'
+ZSH_HIGHLIGHT_STYLES[globbing]='fg=215'
+ZSH_HIGHLIGHT_STYLES[history-expansion]='fg=131'
+ZSH_HIGHLIGHT_STYLES[single-hyphen-option]='fg=110'
+ZSH_HIGHLIGHT_STYLES[double-hyphen-option]='fg=110'
+ZSH_HIGHLIGHT_STYLES[back-quoted-argument]='fg=215'
+ZSH_HIGHLIGHT_STYLES[single-quoted-argument]='fg=252'
+ZSH_HIGHLIGHT_STYLES[double-quoted-argument]='fg=252'
+ZSH_HIGHLIGHT_STYLES[dollar-quoted-argument]='fg=140'
+ZSH_HIGHLIGHT_STYLES[assign]='fg=67'
+ZSH_HIGHLIGHT_STYLES[redirection]='fg=179'
+ZSH_HIGHLIGHT_STYLES[comment]='fg=7e8e9e,italic'
+
+### zsh-history-substring-search Nightfly Theme ###
+# Matches are highlighted with a soft blue foreground and dark navy background
+export ZSH_HIGHLIGHT_HISTORY_SUBSTRING_FG=81
+export ZSH_HIGHLIGHT_HISTORY_SUBSTRING_BG=1d3b53
+
+### zsh-autosuggestions Nightfly Theme ###
+# Suggestions are shown in muted gray-blue
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=7e8e9e"
