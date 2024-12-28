4722ef7823212519c9a83937bdef614f62e80758
Zach Thieme <zach@techsage.org>
2024-12-28 09:54:27 -0800

    not sure

--- dot_config/nvim/lua/zach/core/options.lua
+++ dot_config/nvim/lua/zach/core/options.lua
@@ -27,6 +27,7 @@ opt.termguicolors = true
 opt.background = "dark" -- colorschemes that can be light or dark will be made dark
 opt.signcolumn = "yes" -- show sign column so that text doesn't shift
 
+
 -- cursor changes
 -- opt.guicursor = "n-v-i-c:hor20"
 -- opt.guicursor = ""
