17de5c221ff36c1fce360bafb90f60eb95f490f5
Zach Thieme <zach@techsage.org>
2024-12-30 19:22:17 -0800

    added a few more mini modules

--- dot_config/nvim/lua/zach/plugins/mini.lua
+++ dot_config/nvim/lua/zach/plugins/mini.lua
@@ -4,9 +4,11 @@
   config = function()
     -- Enable mini.nvim modules
     require("mini.ai").setup()
-    require('mini.cursorword').setup()
-	  require('mini.comment').setup()
-    require('mini.surround').setup()
     require('mini.align').setup()
+	  require('mini.comment').setup()
+    require('mini.cursorword').setup()
+    require('mini.indentscope').setup()
+    require('mini.surround').setup()
+    require('mini.trailspace').setup()
   end,
 }
