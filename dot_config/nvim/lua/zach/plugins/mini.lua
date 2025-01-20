ca9d48e386248a716327a8f8eee3d2ec117a49b8
Zach Thieme <zach@techsage.org>
2025-01-20 10:42:22 -0800

    adding auto close braces

--- dot_config/nvim/lua/zach/plugins/mini.lua
+++ dot_config/nvim/lua/zach/plugins/mini.lua
@@ -2,13 +2,14 @@
   "echasnovski/mini.nvim",
   version = "*", -- Use the latest stable version
   config = function()
-    -- Enable mini.nvim modules
-    require("mini.ai").setup()
-    require('mini.align').setup()
-	  require('mini.comment').setup()
-    require('mini.cursorword').setup()
-    require('mini.indentscope').setup()
-    require('mini.surround').setup()
-    require('mini.trailspace').setup()
+     -- Enable mini.nvim modules
+     require("mini.ai").setup()
+     require("mini.pairs").setup()
+     require('mini.align').setup()
+     require('mini.comment').setup()
+     require('mini.cursorword').setup()
+     require('mini.indentscope').setup()
+     require('mini.surround').setup()
+     require('mini.trailspace').setup()
   end,
 }
