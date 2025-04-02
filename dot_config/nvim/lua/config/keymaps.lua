09555cd19865e9423dc716570fcbe768815d9ad7
Zach Thieme <zach@techsage.org>
2025-04-02 15:02:43 -0700

    udating a keymap

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set(
+  "n",
+  "<leader>wn",
+  "<cmd>Calendar -view=year -split=vertical -width=27 -position=right<cr><bar><cmd>Trouble todo<cr>",
+  { desc = "Setup my notes environment." }
+)
