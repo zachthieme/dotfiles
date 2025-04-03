e3cc573cbd6c7045eb0563f5caf7ba0057677a41
Zach Thieme <zach@techsage.org>
2025-04-02 19:49:27 -0700

    still

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -7,25 +7,6 @@ vim.keymap.set(
 	"<cmd>cd ~/Dropbox/vimwiki<cr><bar><cmd>VimwikiIndex<cr><bar><cmd>Calendar -view=year -split=vertical -width=27 -position=right<cr><bar><cmd>wincmd h<cr><bar><cmd>Trouble todo<cr>",
 	{ desc = "Setup my notes environment." }
 )
---
--- vim.keymap.set("n", "<leader>td", function()
--- 	local line = vim.api.nvim_get_current_line()
--- 	local date = os.date("%Y-%m-%d")
---
--- 	if line:match("^%s*TODO") then
--- 		-- Replace TODO with DONE and add completed date
--- 		line = line:gsub("TODO", "DONE", 1)
--- 		if not line:match("completed:%d%d%d%d%-%d%d%-%d%d") then
--- 			line = line .. " completed:" .. date
--- 		end
--- 	elseif line:match("^%s*DONE") then
--- 		-- Replace DONE with TODO and remove completed date
--- 		line = line:gsub("DONE", "TODO", 1)
--- 		line = line:gsub("%s+completed:%d%d%d%d%-%d%d%-%d%d", "")
--- 	end
---
--- 	vim.api.nvim_set_current_line(line)
--- end, { desc = "Toggle TODO/DONE", noremap = true, silent = true })
 vim.keymap.set("n", "<leader>td", function()
 	local line = vim.api.nvim_get_current_line()
 	local date = os.date("%Y-%m-%d")
@@ -38,12 +19,14 @@ vim.keymap.set("n", "<leader>td", function()
 		line = line:gsub("^%s*DONE", restore, 1)
 		line = line:gsub("%s+completed:%d%d%d%d%-%d%d%-%d%d", "")
 		line = line:gsub("%s+original:(TODO|TODAY)", "")
-	elseif line:match("^%s*TODO") or line:match("^%s*TODAY") then
-		local keyword = line:match("^%s*(TODO|TODAY)")
-
-		line = line:gsub("^%s*" .. keyword, "DONE", 1)
-		if not line:match("completed:%d%d%d%d%-%d%d%-%d%d") then
-			line = line .. " completed:" .. date .. " original:" .. keyword
+	else
+		-- Match keyword from the beginning of the line with optional leading spaces
+		local keyword = line:match("^%s*(TODO)") or line:match("^%s*(TODAY)")
+		if keyword then
+			line = line:gsub("^%s*" .. keyword, "DONE", 1)
+			if not line:match("completed:%d%d%d%d%-%d%d%-%d%d") then
+				line = line .. " completed:" .. date .. " original:" .. keyword
+			end
 		end
 	end
 
