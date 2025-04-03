a84827bf5f7f5407b76d5331522b32e500f0801c
Zach Thieme <zach@techsage.org>
2025-04-02 19:33:01 -0700

    making it work with today as well

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,28 +1,57 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set(
-	"n",
-	"<leader>wn",
-	"<cmd>cd ~/Dropbox/vimwiki<cr><bar><cmd>VimwikiIndex<cr><bar><cmd>Calendar -view=year -split=vertical -width=27 -position=right<cr><bar><cmd>wincmd h<cr><bar><cmd>Trouble todo<cr>",
-	{ desc = "Setup my notes environment." }
-)
+-- vim.keymap.set(
+-- 	"n",
+-- 	"<leader>wn",
+-- 	"<cmd>cd ~/Dropbox/vimwiki<cr><bar><cmd>VimwikiIndex<cr><bar><cmd>Calendar -view=year -split=vertical -width=27 -position=right<cr><bar><cmd>wincmd h<cr><bar><cmd>Trouble todo<cr>",
+-- 	{ desc = "Setup my notes environment." }
+-- )
+--
+-- vim.keymap.set("n", "<leader>td", function()
+-- 	local line = vim.api.nvim_get_current_line()
+-- 	local date = os.date("%Y-%m-%d")
+--
+-- 	if line:match("^%s*TODO") then
+-- 		-- Replace TODO with DONE and add completed date
+-- 		line = line:gsub("TODO", "DONE", 1)
+-- 		if not line:match("completed:%d%d%d%d%-%d%d%-%d%d") then
+-- 			line = line .. " completed:" .. date
+-- 		end
+-- 	elseif line:match("^%s*DONE") then
+-- 		-- Replace DONE with TODO and remove completed date
+-- 		line = line:gsub("DONE", "TODO", 1)
+-- 		line = line:gsub("%s+completed:%d%d%d%d%-%d%d%-%d%d", "")
+-- 	end
+--
+-- 	vim.api.nvim_set_current_line(line)
+-- end, { desc = "Toggle TODO/DONE", noremap = true, silent = true })
 
 vim.keymap.set("n", "<leader>td", function()
 	local line = vim.api.nvim_get_current_line()
 	local date = os.date("%Y-%m-%d")
 
-	if line:match("^%s*TODO") then
-		-- Replace TODO with DONE and add completed date
-		line = line:gsub("TODO", "DONE", 1)
-		if not line:match("completed:%d%d%d%d%-%d%d%-%d%d") then
-			line = line .. " completed:" .. date
-		end
-	elseif line:match("^%s*DONE") then
-		-- Replace DONE with TODO and remove completed date
-		line = line:gsub("DONE", "TODO", 1)
+	-- Check for DONE state
+	if line:match("^%s*DONE") then
+		-- Check if the original state was TODO or TODAY (stored as a comment)
+		local original = line:match("original:(TODO|TODAY)")
+		local restore = original or "TODO" -- fallback to TODO
+
+		-- Replace DONE with the original, and remove metadata
+		line = line:gsub("^%s*DONE", restore, 1)
 		line = line:gsub("%s+completed:%d%d%d%d%-%d%d%-%d%d", "")
+		line = line:gsub("%s+original:(TODO|TODAY)", "")
+	elseif line:match("^%s*TODO") or line:match("^%s*TODAY") then
+		local original = line:match("^(%s*)(TODO|TODAY)")
+		local prefix = original and original:match("^%s*") or ""
+		local keyword = line:match("^%s*(TODO|TODAY)")
+
+		-- Replace TODO or TODAY with DONE and add metadata
+		line = line:gsub("^%s*" .. keyword, "DONE", 1)
+		if not line:match("completed:%d%d%d%d%-%d%d%-%d%d") then
+			line = line .. " completed:" .. date .. " original:" .. keyword
+		end
 	end
 
 	vim.api.nvim_set_current_line(line)
-end, { desc = "Toggle TODO/DONE", noremap = true, silent = true })
+end, { desc = "Toggle TODO/TODAY ↔ DONE", noremap = true, silent = true })
