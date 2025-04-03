2787a26fab516b7dfa4be6257c0ae690d658167d
Zach Thieme <zach@techsage.org>
2025-04-02 21:54:49 -0700

    cleaning up

--- dot_config/nvim/lua/plugins/todo-comments.lua
+++ dot_config/nvim/lua/plugins/todo-comments.lua
@@ -1,18 +1,17 @@
 return {
-	"folke/todo-comments.nvim",
-	opts = {
-		highlight = {
-			-- This will highlight TODO or TODO: — colon is optional
-			pattern = [[.*<(KEYWORDS)[:]?\s*]],
-		},
-		search = {
-			-- This will match TODO or TODO: in search and Trouble
-			pattern = [[\b(KEYWORDS):?]],
-		},
-		keywords = {
-			TODO = {
-				alt = { "TODO", "TODAY", "DELEGATED" },
-			},
-		},
-	},
+  "folke/todo-comments.nvim",
+  opts = {
+    highlight = {
+      pattern = [[.*<(KEYWORDS)[:]?\s*]],
+    },
+    search = {
+      -- This will match TODO or TODO: in search and Trouble
+      pattern = [[\b(KEYWORDS):?]],
+    },
+    keywords = {
+      TODO = {
+        alt = { "TODO", "TODAY", "DELEGATED" },
+      },
+    },
+  },
 }
