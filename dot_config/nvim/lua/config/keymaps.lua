1e2e5ce147e9f97517eaa618679fd6c4afb27456
Zach Thieme <zach@techsage.org>
2025-04-02 19:35:37 -0700

    accidentally commented out keymap

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,12 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
--- vim.keymap.set(
--- 	"n",
--- 	"<leader>wn",
--- 	"<cmd>cd ~/Dropbox/vimwiki<cr><bar><cmd>VimwikiIndex<cr><bar><cmd>Calendar -view=year -split=vertical -width=27 -position=right<cr><bar><cmd>wincmd h<cr><bar><cmd>Trouble todo<cr>",
--- 	{ desc = "Setup my notes environment." }
--- )
+vim.keymap.set(
+	"n",
+	"<leader>wn",
+	"<cmd>cd ~/Dropbox/vimwiki<cr><bar><cmd>VimwikiIndex<cr><bar><cmd>Calendar -view=year -split=vertical -width=27 -position=right<cr><bar><cmd>wincmd h<cr><bar><cmd>Trouble todo<cr>",
+	{ desc = "Setup my notes environment." }
+)
 --
 -- vim.keymap.set("n", "<leader>td", function()
 -- 	local line = vim.api.nvim_get_current_line()
