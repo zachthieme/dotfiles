1881a100fb3eb2743494b267300abc8d3a39fc3f
Zach Thieme <zach@techsage.org>
2025-04-02 20:57:09 -0700

    added delegated

--- dot_config/nvim/lua/plugins/todo-comments.lua
+++ dot_config/nvim/lua/plugins/todo-comments.lua
@@ -1,18 +1,18 @@
 return {
-  "folke/todo-comments.nvim",
-  opts = {
-    highlight = {
-      -- This will highlight TODO or TODO: — colon is optional
-      pattern = [[.*<(KEYWORDS)[:]?\s*]],
-    },
-    search = {
-      -- This will match TODO or TODO: in search and Trouble
-      pattern = [[\b(KEYWORDS):?]],
-    },
-    keywords = {
-      TODO = {
-        alt = { "TODO", "TODAY" },
-      },
-    },
-  },
+	"folke/todo-comments.nvim",
+	opts = {
+		highlight = {
+			-- This will highlight TODO or TODO: — colon is optional
+			pattern = [[.*<(KEYWORDS)[:]?\s*]],
+		},
+		search = {
+			-- This will match TODO or TODO: in search and Trouble
+			pattern = [[\b(KEYWORDS):?]],
+		},
+		keywords = {
+			TODO = {
+				alt = { "TODO", "TODAY", "DELEGATED" },
+			},
+		},
+	},
 }
