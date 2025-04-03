9e56adfd56a532232e192b0712bfdd31008570bc
Zach Thieme <zach@techsage.org>
2025-04-02 20:35:11 -0700

    removing snippet

--- dot_config/nvim/lua/plugins/snippets.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"L3MON4D3/LuaSnip",
-	opts = function(_, opts)
-		require("luasnip.loaders.from_lua").lazy_load({
-			paths = { vim.fn.stdpath("config") .. "/lua/snippets" },
-		})
-	end,
-}
