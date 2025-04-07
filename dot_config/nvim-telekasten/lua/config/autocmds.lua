1c8de9cc55bbe43a08422e61900c8c3cca16de3e
Zach Thieme <zach@techsage.org>
2025-04-06 20:18:09 -0700

    adding some tweaks to the highlight line

--- dot_config/nvim-telekasten/lua/config/autocmds.lua
+++ dot_config/nvim-telekasten/lua/config/autocmds.lua
@@ -7,9 +7,9 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-  pattern = vim.fn.expand("~/Dropbox/notes") .. "/**",
+  pattern = vim.fn.expand("~/zettelkasten") .. "/**",
   callback = function()
-    vim.cmd.cd(vim.fn.expand("~/Dropbox/notes"))
+    vim.cmd.cd(vim.fn.expand("~/zettelkasten"))
   end,
 })
 
@@ -17,5 +17,29 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = "telekasten",
   callback = function()
     vim.b.minipairs_disable = true
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "InsertEnter" }, {
+  callback = function()
+    vim.cmd("highlight CursorLine guibg=#4c2c2c") -- Insert mode color
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "InsertLeave" }, {
+  callback = function()
+    vim.cmd("highlight CursorLine guibg=#3c4048") -- Normal mode color
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "VisualEnter" }, {
+  callback = function()
+    vim.cmd("highlight CursorLine guibg=#524f67") -- Visual mode color
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "VisualLeave" }, {
+  callback = function()
+    vim.cmd("highlight CursorLine guibg=#3c4048") -- Reset to Normal mode color
   end,
 })
