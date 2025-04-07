0d87d6bd189a1cfb5e1d6dd08f4ae3bc2be7dd42
Zach Thieme <zach@techsage.org>
2025-04-06 20:25:02 -0700

    testing

--- dot_config/nvim-telekasten/lua/config/autocmds.lua
+++ dot_config/nvim-telekasten/lua/config/autocmds.lua
@@ -7,39 +7,39 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-  pattern = vim.fn.expand("~/Dropbox/notes") .. "/**",
-  callback = function()
-    vim.cmd.cd(vim.fn.expand("~/Dropbox/notes"))
-  end,
+	pattern = vim.fn.expand("~/Dropbox/notes") .. "/**",
+	callback = function()
+		vim.cmd.cd(vim.fn.expand("~/Dropbox/notes"))
+	end,
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "telekasten",
-  callback = function()
-    vim.b.minipairs_disable = true
-  end,
+	pattern = "telekasten",
+	callback = function()
+		vim.b.minipairs_disable = true
+	end,
 })
 
-vim.api.nvim_create_autocmd({ "InsertEnter" }, {
-  callback = function()
-    vim.cmd("highlight CursorLine guibg=#4c2c2c") -- Insert mode color
-  end,
-})
-
-vim.api.nvim_create_autocmd({ "InsertLeave" }, {
-  callback = function()
-    vim.cmd("highlight CursorLine guibg=#3c4048") -- Normal mode color
-  end,
-})
-
-vim.api.nvim_create_autocmd({ "VisualEnter" }, {
-  callback = function()
-    vim.cmd("highlight CursorLine guibg=#524f67") -- Visual mode color
-  end,
-})
-
-vim.api.nvim_create_autocmd({ "VisualLeave" }, {
-  callback = function()
-    vim.cmd("highlight CursorLine guibg=#3c4048") -- Reset to Normal mode color
-  end,
-})
+-- vim.api.nvim_create_autocmd({ "InsertEnter" }, {
+--   callback = function()
+--     vim.cmd("highlight CursorLine guibg=#4c2c2c") -- Insert mode color
+--   end,
+-- })
+--
+-- vim.api.nvim_create_autocmd({ "InsertLeave" }, {
+--   callback = function()
+--     vim.cmd("highlight CursorLine guibg=#3c4048") -- Normal mode color
+--   end,
+-- })
+--
+-- vim.api.nvim_create_autocmd({ "VisualEnter" }, {
+--   callback = function()
+--     vim.cmd("highlight CursorLine guibg=#524f67") -- Visual mode color
+--   end,
+-- })
+--
+-- vim.api.nvim_create_autocmd({ "VisualLeave" }, {
+--   callback = function()
+--     vim.cmd("highlight CursorLine guibg=#3c4048") -- Reset to Normal mode color
+--   end,
+-- })
