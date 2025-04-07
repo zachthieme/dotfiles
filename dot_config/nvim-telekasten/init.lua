aceeb614791b77c30b87e3ef9859f0f9a19fd9b7
Zach Thieme <zach@techsage.org>
2025-04-06 20:01:38 -0700

    remving un-needed stuff

--- dot_config/nvim-telekasten/init.lua
+++ dot_config/nvim-telekasten/init.lua
@@ -1,8 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
--- local home = vim.fn.expand("~/zettelkasten")
--- require("telekasten").setup({
---   home = home,
---   template_new_daily = home .. "/" .. "templates/daily.md",
--- })
