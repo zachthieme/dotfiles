5c8a2b52a81a9cb1904486b770b975b02659fcd1
Zach Thieme <zach@techsage.org>
2025-04-14 16:00:28 -0700

    tweaking todo's

--- dot_config/nvim-obsidian/lua/plugins/obsidian.lua
+++ dot_config/nvim-obsidian/lua/plugins/obsidian.lua
@@ -39,6 +39,17 @@ return {
     daily_notes = {
       template = "~/.config/nvim-obsidian/templates/daily.md",
     },
+    ui = {
+      enable = true,  -- set to false to disable all additional syntax features
+    update_debounce = 200,  -- update delay after a text change (in milliseconds)
+    max_file_length = 5000,  -- disable UI features for files with more than this many lines
+    -- Define how various check-boxes are displayed
+    checkboxes = {
+      [" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
+      ["x"] = { char = "", hl_group = "ObsidianDone" },
+      [">"] = { char = "", hl_group = "ObsidianRightArrow" },
+      ["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
+    },
   },
   completion = {
     nvim_cmp = true,
