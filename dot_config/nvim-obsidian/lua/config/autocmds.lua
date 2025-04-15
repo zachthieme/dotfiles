4117f43c00968266abaae019748a2922693c50a9
Zach Thieme <zach@techsage.org>
2025-04-14 21:24:05 -0700

    adding code folding for markdown

--- dot_config/nvim-obsidian/lua/config/autocmds.lua
+++ dot_config/nvim-obsidian/lua/config/autocmds.lua
@@ -7,18 +7,26 @@
 -- })
 --
 vim.api.nvim_create_autocmd("VimEnter", {
-  callback = function()
-    if vim.fn.argc() == 0 then
-      vim.cmd("cd ~/Dropbox/vaults/work")
-    end
-  end,
+	callback = function()
+		if vim.fn.argc() == 0 then
+			vim.cmd("cd ~/Dropbox/vaults/work")
+		end
+	end,
 })
 
 vim.api.nvim_create_autocmd("InsertLeave", {
-  pattern = "*.md",
-  callback = function()
-    vim.cmd("silent! write")
-    print("saved")
-  end,
-  desc = "Auto-save Markdown files on InsertLeave",
+	pattern = "*.md",
+	callback = function()
+		vim.cmd("silent! write")
+		print("saved")
+	end,
+	desc = "Auto-save Markdown files on InsertLeave",
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "markdown",
+	callback = function()
+		vim.opt_local.foldmethod = "expr"
+		vim.opt_local.foldexpr = "nvim_treesitter#foldexpr()"
+	end,
 })
