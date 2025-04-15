4a374908394dd1083c1cee3e83ee68289362fae4
Zach Thieme <zach@techsage.org>
2025-04-15 15:34:31 -0700

    removing colorscheme

--- dot_config/nvim-obsidian/lua/plugins/colorscheme.lua
+++ dot_config/nvim-obsidian/lua/plugins/colorscheme.lua
@@ -1,3 +1,4 @@
+return {}
 -- return {
 --   { "rose-pine/neovim", name = "rose-pine", lazy = false, priority = 1000 },
 --   { "catppuccin/nvim", name = "catppuccin", lazy = false, priority = 1000 },
