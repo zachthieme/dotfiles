d302080e9460515fede03e3c80671147ca43ee1b
Zach Thieme <zach@techsage.org>
2025-04-17 23:22:55 -0700

    removing legacy code

--- dot_config/nvim-obsidian/init.lua
+++
@@ -1,4 +0,0 @@
-require("config.keymaps")
-require("config.autocmds")
-require("config.options")
-require("config.lazy")
--- dot_config/nvim-obsidian/lua/config/options.lua
+++
@@ -1,3 +0,0 @@
-vim.opt.foldlevel = 99
-vim.opt.termguicolors = true
-vim.cmd.colorscheme("habamax")
