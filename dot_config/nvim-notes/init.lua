b03d6439a80253f8415e4e434869045044ec3d02
Zach Thieme <zach@techsage.org>
2025-04-22 13:28:29 -0700

    added keymap for zenmode

--- dot_config/nvim-notes/init.lua
+++ dot_config/nvim-notes/init.lua
@@ -40,6 +40,7 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right win
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 vim.keymap.set("i", "<M-BS>", "<C-w>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>z", ":lua Snacks.zen()<CR>", { desc = "Toggle Zen mode" })
 
 -- 3. Kept one function
 vim.schedule(function()
