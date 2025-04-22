aa52e448ef087776d0919671d951e3a538d66ab9
Zach Thieme <zach@techsage.org>
2025-04-22 11:05:44 -0700

    added delete previous word shortcut

--- dot_config/nvim-notes/init.lua
+++ dot_config/nvim-notes/init.lua
@@ -39,6 +39,7 @@ vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left wind
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+vim.keymap.set("i", "<M-Left>", "<C-w>", { noremap = true, silent = true })
 
 -- 3. Kept one function
 vim.schedule(function()
