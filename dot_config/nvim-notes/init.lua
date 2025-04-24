1a351101b1126ace7e754b989633d7778de3d45b
Zach Thieme <zach@techsage.org>
2025-04-23 19:53:23 -0700

    added kanagawa

--- dot_config/nvim-notes/init.lua
+++ dot_config/nvim-notes/init.lua
@@ -474,4 +474,13 @@ require("lazy").setup({
 			extensions = {},
 		},
 	},
+	{
+		"rebelot/kanagawa.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("kanagawa").setup({})
+			vim.cmd("colorscheme kanagawa")
+		end,
+	},
 })
