1d1f81730eed753f104a20e0ba49576e26011b2f
Zach Thieme <zach@techsage.org>
2025-05-05 22:11:35 -0700

    added spotify tui

--- dot_config/nix-darwin/flake.nix
+++ dot_config/nix-darwin/flake.nix
@@ -7,12 +7,18 @@
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  outputs =
+    inputs@{
+      self,
+      nix-darwin,
+      nixpkgs,
+    }:
     let
-      configuration = { pkgs, ... }: {
-        # List packages installed in system profile. To search by name, run:
-        environment.systemPackages =
-          [
+      configuration =
+        { pkgs, ... }:
+        {
+          # List packages installed in system profile. To search by name, run:
+          environment.systemPackages = [
             pkgs.bat
             pkgs.curl
             pkgs.dotnetCorePackages.dotnet_9.runtime
@@ -41,66 +47,67 @@
             pkgs.zsh
           ];
 
-        homebrew = {
-          enable = true;
-          # onActivation.cleanup = "uninstall";
+          homebrew = {
+            enable = true;
+            # onActivation.cleanup = "uninstall";
 
-          taps = [
-            "FelixKratz/formulae"
-          ];
-          brews = [
-            "oh-my-posh"
-            "FelixKratz/formulae/borders"
-            "FelixKratz/formulae/sketchybar"
-          ];
-          casks = [
-            "brave-browser"
-            "ghostty"
-            "nikitabobko/tap/aerospace"
-            "raycast"
-            "spotify"
-            "zed"
-          ];
+            taps = [
+              "FelixKratz/formulae"
+            ];
+            brews = [
+              "FelixKratz/formulae/borders"
+              "FelixKratz/formulae/sketchybar"
+              "oh-my-posh"
+              "spotify-player"
+            ];
+            casks = [
+              "brave-browser"
+              "ghostty"
+              "nikitabobko/tap/aerospace"
+              "raycast"
+              "spotify"
+              "zed"
+            ];
+          };
+
+          # Necessary for using flakes on this system.
+          nix.settings.experimental-features = "nix-command flakes";
+          nix.enable = false;
+
+          # Set Git commit hash for darwin-version.
+          system.configurationRevision = self.rev or self.dirtyRev or null;
+
+          # configuring mac os
+          # use touchid in terminal
+          security.pam.services.sudo_local.touchIdAuth = true;
+
+          system.defaults = {
+            dock.autohide = true;
+            finder.AppleShowAllExtensions = true;
+            finder.FXPreferredViewStyle = "clmv";
+            screencapture.location = "~/Pictures/screenshots";
+            screensaver.askForPasswordDelay = 10;
+
+            # aerospace configuration
+            dock.mru-spaces = false;
+            dock.expose-group-apps = true;
+            spaces.spans-displays = true;
+            NSGlobalDomain.NSWindowShouldDragOnGesture = true;
+          };
+          system.keyboard = {
+            enableKeyMapping = true;
+            remapCapsLockToEscape = true;
+          };
+
+          programs.zsh.enable = true;
+
+          # Used for backwards compatibility, please read the changelog before changing.
+          # $ darwin-rebuild changelog
+          system.stateVersion = 6;
+
+          # The platform the configuration will be used on.
+          nixpkgs.hostPlatform = "aarch64-darwin";
         };
-
-        # Necessary for using flakes on this system.
-        nix.settings.experimental-features = "nix-command flakes";
-        nix.enable = false;
-
-        # Set Git commit hash for darwin-version.
-        system.configurationRevision = self.rev or self.dirtyRev or null;
-
-        # configuring mac os
-        # use touchid in terminal
-        security.pam.services.sudo_local.touchIdAuth = true;
-
-        system.defaults = {
-          dock.autohide = true;
-          finder.AppleShowAllExtensions = true;
-          finder.FXPreferredViewStyle = "clmv";
-          screencapture.location = "~/Pictures/screenshots";
-          screensaver.askForPasswordDelay = 10;
-
-          # aerospace configuration
-          dock.mru-spaces = false;
-          dock.expose-group-apps = true;
-          spaces.spans-displays = true;
-          NSGlobalDomain.NSWindowShouldDragOnGesture = true;
-        };
-        system.keyboard = {
-          enableKeyMapping = true;
-          remapCapsLockToEscape = true;
-        };
-
-        programs.zsh.enable = true;
-
-        # Used for backwards compatibility, please read the changelog before changing.
-        # $ darwin-rebuild changelog
-        system.stateVersion = 6;
-
-        # The platform the configuration will be used on.
-        nixpkgs.hostPlatform = "aarch64-darwin";
-      };
     in
     {
       # Build darwin flake using:
