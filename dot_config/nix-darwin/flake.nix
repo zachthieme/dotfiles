0af5ef57b313e2d9d03177e7e4d87c7b025642ab
Zach Thieme <zach@techsage.org>
2025-05-05 21:25:53 -0700

    added nixfmt to flake

--- dot_config/nix-darwin/flake.nix
+++ dot_config/nix-darwin/flake.nix
@@ -9,95 +9,97 @@
 
   outputs = inputs@{ self, nix-darwin, nixpkgs }:
     let
-    configuration = { pkgs, ... }: {
-# List packages installed in system profile. To search by name, run:
-      environment.systemPackages =
-        [ 
-          pkgs.bat
-          pkgs.curl
-          pkgs.dotnetCorePackages.dotnet_9.runtime
-          pkgs.dotnetCorePackages.dotnet_9.sdk
-          pkgs.eza
-          pkgs.fd
-          pkgs.fzf
-          pkgs.gh
-          pkgs.git
-          pkgs.go
-          pkgs.gotools
-          pkgs.jq
-          pkgs.mosh
-          pkgs.neovim
-          pkgs.nodejs_23
-          pkgs.pandoc
-          pkgs.pass
-          pkgs.python3
-          pkgs.ripgrep
-          pkgs.tmux
-          pkgs.vim
-          pkgs.wget
-          pkgs.yazi
-          pkgs.zoxide
-          pkgs.zsh
-        ];
+      configuration = { pkgs, ... }: {
+        # List packages installed in system profile. To search by name, run:
+        environment.systemPackages =
+          [
+            pkgs.bat
+            pkgs.curl
+            pkgs.dotnetCorePackages.dotnet_9.runtime
+            pkgs.dotnetCorePackages.dotnet_9.sdk
+            pkgs.eza
+            pkgs.fd
+            pkgs.fzf
+            pkgs.gh
+            pkgs.git
+            pkgs.go
+            pkgs.gotools
+            pkgs.jq
+            pkgs.mosh
+            pkgs.neovim
+            pkgs.nixfmt-rfc-style
+            pkgs.nodejs_23
+            pkgs.pandoc
+            pkgs.pass
+            pkgs.python3
+            pkgs.ripgrep
+            pkgs.tmux
+            pkgs.vim
+            pkgs.wget
+            pkgs.yazi
+            pkgs.zoxide
+            pkgs.zsh
+          ];
 
-      homebrew = {
-        enable = true;
-       # onActivation.cleanup = "uninstall";
+        homebrew = {
+          enable = true;
+          # onActivation.cleanup = "uninstall";
 
-        taps = [ ];
-        brews = [ "oh-my-posh" ];
-        casks = [ "raycast" 
-          "brave-browser"
-          "zed"
-          "ghostty"
-          "nikitabobko/tap/aerospace"
-        ];
+          taps = [ ];
+          brews = [ "oh-my-posh" ];
+          casks = [
+            "raycast"
+            "brave-browser"
+            "zed"
+            "ghostty"
+            "nikitabobko/tap/aerospace"
+          ];
+        };
+
+        # Necessary for using flakes on this system.
+        nix.settings.experimental-features = "nix-command flakes";
+        nix.enable = false;
+
+        # Set Git commit hash for darwin-version.
+        system.configurationRevision = self.rev or self.dirtyRev or null;
+
+        # configuring mac os
+        # use touchid in terminal
+        security.pam.services.sudo_local.touchIdAuth = true;
+
+        system.defaults = {
+          dock.autohide = true;
+          finder.AppleShowAllExtensions = true;
+          finder.FXPreferredViewStyle = "clmv";
+          screencapture.location = "~/Pictures/screenshots";
+          screensaver.askForPasswordDelay = 10;
+
+          # aerospace configuration
+          dock.mru-spaces = false;
+          dock.expose-group-apps = true;
+          spaces.spans-displays = true;
+          NSGlobalDomain.NSWindowShouldDragOnGesture = true;
+        };
+        system.keyboard = {
+          enableKeyMapping = true;
+          remapCapsLockToEscape = true;
+        };
+
+        programs.zsh.enable = true;
+
+        # Used for backwards compatibility, please read the changelog before changing.
+        # $ darwin-rebuild changelog
+        system.stateVersion = 6;
+
+        # The platform the configuration will be used on.
+        nixpkgs.hostPlatform = "aarch64-darwin";
       };
-
-# Necessary for using flakes on this system.
-nix.settings.experimental-features = "nix-command flakes";
-nix.enable = false;
-
-# Set Git commit hash for darwin-version.
-system.configurationRevision = self.rev or self.dirtyRev or null;
-
-# configuring mac os
-# use touchid in terminal
-      security.pam.services.sudo_local.touchIdAuth = true;
-
-      system.defaults = {
-        dock.autohide = true;
-        finder.AppleShowAllExtensions = true;
-        finder.FXPreferredViewStyle = "clmv";
-        screencapture.location = "~/Pictures/screenshots";
-        screensaver.askForPasswordDelay = 10;
-
-# aerospace configuration
-        dock.mru-spaces = false;
-        dock.expose-group-apps = true;
-        spaces.spans-displays = true;
-        NSGlobalDomain.NSWindowShouldDragOnGesture = true;
+    in
+    {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#simple
+      darwinConfigurations."Cortex" = nix-darwin.lib.darwinSystem {
+        modules = [ configuration ];
       };
-      system.keyboard = { 
-        enableKeyMapping = true;
-        remapCapsLockToEscape = true;
-      };
-
-      programs.zsh.enable = true;
-
-# Used for backwards compatibility, please read the changelog before changing.
-# $ darwin-rebuild changelog
-      system.stateVersion = 6;
-
-# The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
     };
-  in
-  {
-# Build darwin flake using:
-# $ darwin-rebuild build --flake .#simple
-    darwinConfigurations."Cortex" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
-    };
-  };
 }
