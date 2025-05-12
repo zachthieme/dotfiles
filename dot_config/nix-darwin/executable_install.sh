c8de331a952598fee5e811c4a912294af20088d4
Zach Thieme <zach@techsage.org>
2025-05-11 18:37:07 -0700

    adding some notes

--- dot_config/nix-darwin/executable_install.sh
+++ dot_config/nix-darwin/executable_install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# figure out in nix
+# add address bar in finder
+# add path bar in finder
+
 # install doom-emacs
 if ! command -v "$HOME/.config/emacs/bin/doom" &>/dev/null; then
   git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
