83fb2d7d40dc1c5572539f8d533d271fbea0db83
Zach Thieme <zach@techsage.org>
2025-05-11 18:40:23 -0700

    more notes

--- dot_config/nix-darwin/executable_install.sh
+++ dot_config/nix-darwin/executable_install.sh
@@ -3,6 +3,7 @@
 # figure out in nix
 # add address bar in finder
 # add path bar in finder
+# add dropbox to the favorites in finder
 
 # install doom-emacs
 if ! command -v "$HOME/.config/emacs/bin/doom" &>/dev/null; then
