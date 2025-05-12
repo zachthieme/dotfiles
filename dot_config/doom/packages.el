5beda4c5db1997f31db241c873fb7b8567e49629
Zach Thieme <zach@techsage.org>
2025-05-11 21:36:53 -0700

    adding better searching for roam doc

--- dot_config/doom/packages.el
+++ dot_config/doom/packages.el
@@ -1,1 +1,10 @@
 (package! org-super-agenda)
+(use-package! consult-org-roam
+  :after org-roam
+  :config
+  (consult-org-roam-mode 1)
+  ;; Optional keybindings
+  (map! :leader
+        "n f" #'consult-org-roam-file-find
+        "n F" #'consult-org-roam-search
+        "n n" #'consult-org-roam-node-find))
