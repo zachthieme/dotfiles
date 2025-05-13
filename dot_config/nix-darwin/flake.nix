b7e9fe8a7916cf55f897f5a5183cab39b58fc36d
Zach Thieme <zach@techsage.org>
2025-05-12 21:58:48 -0700

    minor fix in nix path

--- dot_config/nix-darwin/flake.nix
+++ dot_config/nix-darwin/flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin = {
       url = "github:nix-darwin/nix-darwin/master";
-      nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     # home-manager = {
     #   url = "github:nix-community/home-manager";
