0e58dd46e834efd941e3f8a04a05b55371bed6c3
Zach Thieme <zach@techsage.org>
2025-05-18 21:31:21 -0700

    moving away from chezmoi

--- dot_config/fzf/themes/NightFly.zsh
+++
@@ -1,6 +0,0 @@
-export FZF_DEFAULT_OPTS="
-  --color=fg:#acb4c2,hl:#82aaff
-  --color=fg+:#82aaff,bg+:#1d3b53,hl+:#7fdbca
-  --color=info:#c792ea,prompt:#82aaff,pointer:#f07178
-  --color=marker:#7fdbca,spinner:#ecc48d,header:#7e8e9e
-"
