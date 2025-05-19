ecc30751876940c945773c5857a7bec396d03bc0
Zach Thieme <zach@techsage.org>
2025-05-18 17:56:01 -0700

    updating home.nix to match

--- dot_config/nix-darwin/home.nix
+++ dot_config/nix-darwin/home.nix
@@ -5,8 +5,8 @@
 }:
 
 {
-  home.username = "zach";
-  home.homeDirectory = "/Users/zach";
+  home.username = builtins.getEnv "USER";
+  home.homeDirectory = "/Users/${config.home.username}";
   home.stateVersion = "25.05"; # Adjust based on your nixpkgs version
   home.sessionVariables = {
     EDITOR = "nvim";
