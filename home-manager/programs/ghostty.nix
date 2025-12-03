9911f828585b49961db981d0cb44b49809a82349
Zach Thieme <zach@techsage.org>
2025-12-02 19:05:56 -0800

    fixed opt-alt- parens to work in zellij

--- home-manager/programs/ghostty.nix
+++ home-manager/programs/ghostty.nix
@@ -12,6 +12,8 @@
       # Enable option-as-alt for fish partial completion
       macos-option-as-alt = true;
       keybind = [
+        ''alt+shift+0=text:\x1b)''
+        ''alt+shift+9=text:\x1b(''
         "global:ctrl+grave_accent=toggle_quick_terminal"
         "alt+left=unbind"
         "alt+right=unbind"
