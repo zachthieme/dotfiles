757b171afbc9571486e1466048759f98a6d0e83e
Zach Thieme <zach@techsage.org>
2026-03-01 03:26:29 +0000

    restore pane frames in tasks tab to show pane names

--- home-manager/programs/zellij.nix
+++ home-manager/programs/zellij.nix
@@ -135,10 +135,10 @@ in
             pane size=1 borderless=true {
                 plugin location="zellij:compact-bar"
             }
-            pane borderless=true name="overdue" focus=true command="fish" {
+            pane name="overdue" focus=true command="fish" {
                 args "-c" "overdue"
             }
-            pane borderless=true name="next 3 days" command="fish" {
+            pane name="next 3 days" command="fish" {
                 args "-c" "upcoming 3"
             }
         }
